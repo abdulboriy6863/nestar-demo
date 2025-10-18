ea0243aeea24e4d5013be7881805bb71443de8a8
abdulboriy6863 <abdulboriy6863@gmail.com>
2025-10-18 11:51:52 -0700

    feat: meta tags integrations on document private file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,3 +4,5 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
+
+//app orqali global integrationni amalga oshiramiz
